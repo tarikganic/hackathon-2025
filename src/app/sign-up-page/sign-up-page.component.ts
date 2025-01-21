d7bdc3c0a2465eed8818eb66b710b7eb620e3f3d
tarikganic <riktaganic@gmail.com>
2025-01-21 23:54:03 +0100

    countdown-implementation

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCheckToSlot, faDashboard, faDownload } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,7 +8,33 @@ import { faCheckToSlot, faDashboard, faDownload } from '@fortawesome/free-solid-
   templateUrl: './sign-up-page.component.html',
   styleUrl: './sign-up-page.component.css'
 })
-export class SignUpPageComponent {
+export class SignUpPageComponent implements OnInit{
+  constructor(private ngZone: NgZone) {}
+  countdown: string = ''; 
+
+  ngOnInit(){
+    this.calculateCountdown();
+    this.ngZone.runOutsideAngular(()=>{
+      setInterval(()=>{
+        this.ngZone.run(()=>{
+          this.calculateCountdown();
+        });
+      }, 1000);
+    });
+  }
+
+  calculateCountdown() {
+    const targetDate = new Date('2025-01-24T10:00:00');
+    const now = new Date();
+    const timeRemaining = targetDate.getTime() - now.getTime();
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+    this.countdown = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  }
+
+
   faDownload = faDownload;
   faCheckToSlot = faCheckToSlot
 
